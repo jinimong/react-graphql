e431f7ad677b0072e84cec82c0f824f9e4e116ba
jwkim <phoongdeng@gmail.com>
2021-02-10 16:10:48 +0900

    Add editUser field mutation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -98,6 +98,20 @@ const mutation = new GraphQLObjectType({
           .catch((error) => false);
       },
     },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then((response) => response.data);
+      },
+    },
   },
 });
 
