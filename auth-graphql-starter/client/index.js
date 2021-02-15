634fd44346c1b9075132754fdad17d3b23e6ecc9
jwkim <phoongdeng@gmail.com>
2021-02-16 01:55:15 +0900

    Add RequireAuth HOC for permission checking

--- auth-graphql-starter/client/index.js
+++ auth-graphql-starter/client/index.js
@@ -7,6 +7,7 @@ import App from './components/App';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
 import Dashboard from './components/Dashboard';
+import RequireAuth from './components/RequireAuth';
 
 const client = new ApolloClient({
   dataIdFromObject: (obj) => obj.id,
@@ -19,7 +20,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <Route path="/login" component={LoginForm} />
           <Route path="/signup" component={SignupForm} />
-          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/dashboard" component={RequireAuth(Dashboard)} />
         </Route>
       </Router>
     </ApolloProvider>
